1a2209cf5fca563264093ddd4586b776cf2bd0fd
Anu Khambete <anukhambete@gmail.com>
2018-06-27 21:17:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,3 @@
-
-
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
@@ -8,24 +6,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-
-      search_term = req.params["q"]
-
-      if @@items.include?(search_term)
-        resp.write "#{search_term} is one of our items"
-      else
-        resp.write "Couldn't find #{search_term}"
-      end
-
-    else
-      resp.write "Path Not Found"
+    @@items.each do |item|
+      resp.write "#{item}\n"
     end
- 
+
     resp.finish
   end
 end
